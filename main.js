aa91599235fd1ec31b6f082cbe7094721d12653c
yajat23 <89627077+yajat23@users.noreply.github.com>
2022-05-28 11:47:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+var SpeechRcognition = window.webkitSpeechRecognition;
+
+var recognition = new SpeechRecognition();
+
+function start()
+{
+document.getElementById("textbox").innerHTML = "";
+recognition.start()
+}
+
+recognition.onresult = function(event) {
+    console.log(event);
+
+    var Content = event.results[0][0].transcript = Content;
+    console.log(Content);
+    document.getElementById("textbox").innerHTML = Content;
+speak();
+}
+function speak()
+{
+var synth = window.speechSynthesis;
+speak_data = document.getElementById("textbox");
+var utterThis = new SpeechSynthesisUtterance(speak_data);
+synth.speak(utterThis);
+Webcam.attach(camera);
+}
+
+Webcam.set({
+width:360,
+height:230,
+image_format :'png',
+png_quality:90
+});
+camera = document.getElementById("camera");
